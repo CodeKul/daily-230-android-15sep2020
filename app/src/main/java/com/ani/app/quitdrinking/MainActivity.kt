8618fa0f2c3960cd7fa9454aa2e6961db655c3b7
aniruddha <aniruddha.kudalkar@gmail.com>
2020-09-15 16:09:35 +0530

    UI - activity screen design

--- app/src/main/java/com/ani/app/quitdrinking/MainActivity.kt
+++ app/src/main/java/com/ani/app/quitdrinking/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.ani.app.quitdrinking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val view = View(this) // R.layout.activity_main --> compiled -> view
         setContentView(R.layout.activity_main)
     }
 }
+
