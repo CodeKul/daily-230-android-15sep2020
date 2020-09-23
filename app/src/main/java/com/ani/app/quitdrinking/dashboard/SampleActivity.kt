c84d5c401bf66a1a9d28a408bd843b6fc34076dc
aniruddha <aniruddha.kudalkar@gmail.com>
2020-09-23 12:16:48 +0530

    activity : location - location setting activity

--- app/src/main/java/com/ani/app/quitdrinking/dashboard/SampleActivity.kt
+++ app/src/main/java/com/ani/app/quitdrinking/dashboard/SampleActivity.kt
@@ -2,15 +2,20 @@ package com.ani.app.quitdrinking.dashboard
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.location.LocationManagerCompat
 import com.ani.app.quitdrinking.R
 import kotlinx.android.synthetic.main.activity_sample.*
+
 
 class SampleActivity : AppCompatActivity() {
     private var device : String = "mobile"
 
+    //https://stackoverflow.com/questions/25175522/how-to-enable-location-access-programmatically-in-android/25175756
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
@@ -18,6 +23,7 @@ class SampleActivity : AppCompatActivity() {
         swMb.setOnCheckedChangeListener { buttonView, isChecked ->
             device = if(isChecked) "tv" else "mobile"
             buttonView.text = device
+            if(isChecked) locationSettingsActivity()
         }
 
         btNx.setOnClickListener {
@@ -36,12 +42,31 @@ class SampleActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1234) {
+        if(requestCode == REQUEST_SETTINGS) {
             if(resultCode == RESULT_OK) {
                 swMb.text = data?.extras?.getString("dimensions")
             }
             Log.i("@ani", "You are coming back from Settings Activity")
         }
+        if(requestCode == REQUEST_LOCATION_SETTINGS) {
+            Log.i("@ani", "REsult is ${resultCode}")
+            swMb.text = if(isLocationEnabled()) "enabled" else "disabled"
+            swMb.isChecked = isLocationEnabled()
+        }
+    }
 
+    private fun locationSettingsActivity() {
+        val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivityForResult(callGPSSettingIntent, REQUEST_LOCATION_SETTINGS)
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
+    }
+
+    companion object {
+        const val REQUEST_SETTINGS = 1234
+        const val REQUEST_LOCATION_SETTINGS = 4568
     }
 }
